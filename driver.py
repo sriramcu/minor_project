8f36fecb58f5e808895712d3331fd659e0db13fa
sriram <csriram12345@gmail>
2021-08-12 13:09:20 +0530

    Resolved imports errors

--- driver.py
+++ driver.py
@@ -10,36 +10,28 @@ import numpy as np
 def main():
 
     # bsizes = [100, 200, 300, 400, 500, 600, 700, 800, 900, 1000]
-
-
     pdrops = []
-    delay1s = []
-    delay2s = []
-    sw_pdrops = []
+    delays = []
     throughputs = []
     mode = "dynamic"
     if mode == "static":
         bsizes = [100, 120, 140, 160, 180, 200, 220, 240, 260, 280, 300, 1000, 3000, 10000, 20000]
         for bsize in bsizes:
-            delay1, delay2, pdrop, sw_pdrop, throughput = QueueNet2.simulate_network(bsize)
+            delay, pdrop, throughput = QueueNet2.simulate_network(bsize)
             pdrops.append(pdrop)
-            delay1s.append(delay1)
-            delay2s.append(delay2)
-            sw_pdrops.append(sw_pdrop)
+            delays.append(delay)
             throughputs.append(throughput)
 
     else:
         bsize = 10  # initial
         bsizes = []
         for i in range(12):
-            delay1, delay2, pdrop, sw_pdrop, throughput = QueueNet2.simulate_network(bsize)
+            delay, pdrop, throughput = QueueNet2.simulate_network(bsize)
             pdrops.append(pdrop)
-            delay1s.append(delay1)
-            delay2s.append(delay2)
-            sw_pdrops.append(sw_pdrop)
+            delays.append(delay)
             throughputs.append(throughput)
             reward = 0
-            if (delay1 + delay2) > (delay1s[-1]+delay2s[-1]):
+            if delay > delays[-1]:
                 reward -= 10
             else:
                 reward += 10
@@ -54,33 +46,19 @@ def main():
             print("Buffer size is now ", bsize)
             bsizes.append(bsize)
 
-    # print(delay1s, delay2s, pdrops, sw_pdrops)
-
     plt.figure(1)
     plt.title("Delay")
-    plt.plot(np.array(bsizes), np.array(delay1s)+np.array(delay2s))
+    plt.plot(np.array(bsizes), np.array(delays))
     plt.savefig("graphs/delay.png")
     plt.show()
 
-    #plt.figure(2)
-    #plt.title("Delay 2")
-    #plt.plot(np.array(bsizes), np.array(delay2s))
-    #plt.savefig("graphs/delay2.png")
-    #plt.show()
-
-    plt.figure(3)
+    plt.figure(2)
     plt.title("Packet drop %")
     plt.plot(np.array(bsizes), np.array(pdrops))
     plt.savefig("graphs/pdrop.png")
     plt.show()
 
-    plt.figure(4)
-    plt.title("Switch packet drop %")
-    plt.plot(np.array(bsizes), np.array(sw_pdrops))
-    plt.savefig("graphs/sw_pdrop.png")
-    plt.show()
-
-    plt.figure(5)
+    plt.figure(3)
     plt.title("Throughput")
     plt.plot(np.array(bsizes), np.array(throughputs))
     plt.savefig("graphs/throughput.png")
