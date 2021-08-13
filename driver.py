46f49529cb0580278b0cb47f0db97fa0818aed9c
sriram <csriram12345@gmail>
2021-08-13 22:30:13 +0530

    New Driver Program

--- driver.py
+++ driver.py
@@ -2,36 +2,62 @@ from utils import constants
 from utils.sdnenvironment import SdnEnvironment
 from utils.rl_agent import DQNAgent
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 def main():
-    env = SdnEnvironment()
+    sdn_env = SdnEnvironment()
     state_size = constants.STATE_SIZE
     action_size = constants.ACTION_SIZE
-    agent = DQNAgent(state_size, action_size)
+    rl_agent = DQNAgent(state_size, action_size)
 
     done = False
     batch_size = 32
     episodes = constants.EPISODES
     for e in range(episodes):
-        state = env.reset()
+        state = sdn_env.reset()
         print("EPISODE", e)
         state = np.reshape(state, [1, state_size])
+        delays = []
+        pdrops = []
+        throughputs = []
         for time in range(constants.NUM_SIMULATIONS):
-            print("Episode", e, "Time", time)
             # env.render()
-            action = agent.act(state)
-            next_state, reward, done, _ = env.step(action)
+            action = rl_agent.act(state)
+            next_state, reward, done, _ = sdn_env.step(action)
             reward = reward if not done else -10
             next_state = np.reshape(next_state, [1, state_size])
-            agent.memorize(state, action, reward, next_state, done)
+            rl_agent.memorize(state, action, reward, next_state, done)
             state = next_state
+            delay, pdrop, throughput = list(state[0])
+            delays.append(delay)
+            pdrops.append(pdrop)
+            throughputs.append(throughput)
+
             if done:
                 print("episode: {}/{}, score: {}, e: {:.2}"
-                      .format(e, episodes, time, agent.epsilon))
+                      .format(e, episodes, time, rl_agent.epsilon))
                 break
-            if len(agent.memory) > batch_size:
-                agent.replay(batch_size)
+            if len(rl_agent.memory) > batch_size:
+                rl_agent.replay(batch_size)
+
+        plt.figure(1)
+        plt.title("Delay, episode {}".format(e+1))
+        plt.plot(np.array(delays))
+        plt.savefig("graphs/delay_episode{}.png".format(e+1))
+        plt.show()
+
+        plt.figure(2)
+        plt.title("Packet drop %, episode {}".format(e+1))
+        plt.plot(np.array(pdrops))
+        plt.savefig("graphs/pdrop_episode{}.png".format(e+1))
+        plt.show()
+
+        plt.figure(3)
+        plt.title("Throughput, episode {}".format(e+1))
+        plt.plot(np.array(throughputs))
+        plt.savefig("graphs/throughput_episode{}.png".format(e+1))
+        plt.show()
 
 
 if __name__ == "__main__":
